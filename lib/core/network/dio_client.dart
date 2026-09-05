c437f754ad045f819ce3f7d876941bd6fea63421
Adam_Diass <adamrpdiass@hotmail.com>
2026-09-05 19:25:11 -0300

    feat: add authentication interceptor

--- lib/core/network/dio_client.dart
+++ lib/core/network/dio_client.dart
@@ -1,16 +1,35 @@
 import 'package:dio/dio.dart';
+import 'package:orbytis_atlas/core/storage/secure_storage_service.dart';
 
 import '../config/app_config.dart';
 
 final class DioClient {
-  DioClient()
+  DioClient({required SecureStorageService secureStorageService})
     : dio = Dio(
         BaseOptions(
           baseUrl: AppConfig.apiBaseUrl,
           connectTimeout: const Duration(seconds: 10),
           receiveTimeout: const Duration(seconds: 10),
+          sendTimeout: const Duration(seconds: 10),
           headers: const {'Content-Type': 'application/json'},
         ),
-      );
+      ) {
+    dio.interceptors.add(
+      InterceptorsWrapper(
+        onRequest: (options, handler) async {
+          final isLoginRequest = options.path == '/auth/login';
+
+          if (!isLoginRequest) {
+            final token = await secureStorageService.readAccessToken();
+
+            if (token != null && token.isNotEmpty) {
+              options.headers['Authorization'] = 'Bearer $token';
+            }
+          }
+          handler.next(options);
+        },
+      ),
+    );
+  }
   final Dio dio;
 }
