9e6e127c60993ba1dba88eac6f16a18a50b6dd87
Adam_Diass <adamrpdiass@hotmail.com>
2026-09-06 18:34:19 -0300

    fix: configure local api access on physical device

--- lib/core/config/app_config.dart
+++ lib/core/config/app_config.dart
@@ -1,5 +1,5 @@
 abstract final class AppConfig {
   static const String appName = 'Orbytis Atlas';
 
-  static const String apiBaseUrl = 'http://10.0.2.2:3000';
+  static const String apiBaseUrl = 'http://localhost:3000';
 }
