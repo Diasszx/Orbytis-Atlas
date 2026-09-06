66d5e60a89fbbedecc8cf557ee8cd78a1958911d
Adam_Diass <adamrpdiass@hotmail.com>
2026-09-06 18:49:44 -0300

    feat: configure app routing

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,10 @@
 import 'package:flutter/material.dart';
 import 'package:orbytis_atlas/app/app.dart';
+import 'package:orbytis_atlas/app/router/app_router.dart';
 import 'package:orbytis_atlas/core/network/dio_client.dart';
 import 'package:orbytis_atlas/core/storage/secure_storage_service.dart';
 import 'package:orbytis_atlas/features/auth/datasources/auth_remote_data_source.dart';
 import 'package:orbytis_atlas/features/auth/repositories/auth_repository.dart';
-
-import 'core/config/app_config.dart';
 
 void main() {
   final secureStorageService = SecureStorageService();
@@ -14,10 +13,14 @@ void main() {
 
   final authRemoteDataSource = AuthRemoteDataSource(dioClient: dioClient);
 
+  final appRouter = AppRouter();
+
   final authRepository = AuthRepository(
     remoteDataSource: authRemoteDataSource,
     secureStorageService: secureStorageService,
   );
 
-  runApp(OrbytisAtlasApp(authRepository: authRepository));
+  runApp(
+    OrbytisAtlasApp(authRepository: authRepository, router: appRouter.router),
+  );
 }
