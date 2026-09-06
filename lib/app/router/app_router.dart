ff113fe0795b5fffd7667dff7276f2a00a98369e
Adam_Diass <adamrpdiass@hotmail.com>
2026-09-06 19:21:01 -0300

    feat: protect routes by authentication state

--- lib/app/router/app_router.dart
+++ lib/app/router/app_router.dart
@@ -1,20 +1,42 @@
 import 'package:go_router/go_router.dart';
+import 'package:orbytis_atlas/features/auth/presentation/bloc/auth_bloc.dart';
+import 'package:orbytis_atlas/features/auth/presentation/bloc/auth_state.dart';
 import 'package:orbytis_atlas/features/work_orders/presentation/pages/work_orders_page.dart';
 
 import '../../features/auth/presentation/pages/login_page.dart';
 
 final class AppRouter {
-  AppRouter();
+  AppRouter({required AuthBloc authBloc})
+    : router = GoRouter(
+        initialLocation: '/login',
+        redirect: (context, state) {
+          final authState = authBloc.state;
 
-  final GoRouter router = GoRouter(
-    initialLocation: '/login',
-    routes: [
-      GoRoute(path: '/', redirect: (_, _) => '/login'),
-      GoRoute(path: '/login', builder: (context, state) => const LoginPage()),
-      GoRoute(
-        path: '/work-orders',
-        builder: (context, state) => const WorkOrdersPage(),
-      ),
-    ],
-  );
+          final isAuthenticated = authState is AuthAuthenticated;
+          final isGoingToLogin = state.matchedLocation == '/login';
+
+          if (!isAuthenticated && !isGoingToLogin) {
+            return '/login';
+          }
+
+          if (isAuthenticated && isGoingToLogin) {
+            return 'work-orders';
+          }
+
+          return null;
+        },
+        routes: [
+          GoRoute(path: '/', redirect: (_, _) => '/work-orders'),
+          GoRoute(
+            path: '/login',
+            builder: (context, state) => const LoginPage(),
+          ),
+          GoRoute(
+            path: '/work-orders',
+            builder: (context, state) => const WorkOrdersPage(),
+          ),
+        ],
+      );
+
+  final GoRouter router;
 }
--- lib/main.dart
+++ lib/main.dart
@@ -4,6 +4,7 @@ import 'package:orbytis_atlas/app/router/app_router.dart';
 import 'package:orbytis_atlas/core/network/dio_client.dart';
 import 'package:orbytis_atlas/core/storage/secure_storage_service.dart';
 import 'package:orbytis_atlas/features/auth/datasources/auth_remote_data_source.dart';
+import 'package:orbytis_atlas/features/auth/presentation/bloc/auth_bloc.dart';
 import 'package:orbytis_atlas/features/auth/repositories/auth_repository.dart';
 
 void main() {
@@ -13,14 +14,14 @@ void main() {
 
   final authRemoteDataSource = AuthRemoteDataSource(dioClient: dioClient);
 
-  final appRouter = AppRouter();
-
   final authRepository = AuthRepository(
     remoteDataSource: authRemoteDataSource,
     secureStorageService: secureStorageService,
   );
 
-  runApp(
-    OrbytisAtlasApp(authRepository: authRepository, router: appRouter.router),
-  );
+  final authBloc = AuthBloc(authRepository);
+
+  final appRouter = AppRouter(authBloc: authBloc);
+
+  runApp(OrbytisAtlasApp(authBloc: authBloc, router: appRouter.router));
 }
